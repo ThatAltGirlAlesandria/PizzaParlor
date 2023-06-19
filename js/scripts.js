75c29e4cf6ebfd9e3483db156edd869c711918a8
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-06-19 10:57:01 -0700

    add object function

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function Order(pizzaSize, pizzaCrust, pizzaSauce, pizzaToppings = []) {
+  this.pizzaSize = pizzaSize;
+  this.pizzaCrust = pizzaCrust;
+  this.pizzaSauce = pizzaSauce
+  this.pizzaToppings = pizzaToppings;
+}
