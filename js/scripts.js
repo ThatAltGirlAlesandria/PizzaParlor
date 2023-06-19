0fdbb18428b29e9b015552b675e38cd63feda30e
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-06-19 11:29:24 -0700

    add order submit function.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,16 @@ function Order(pizzaSize, pizzaCrust, pizzaSauce, pizzaToppings = []) {
   this.pizzaSauce = pizzaSauce
   this.pizzaToppings = pizzaToppings;
 }
+
+function handleSubmitOrder(e){
+  e.preventDefault();
+  const pizzSize = document.getElementById("pizza-size").value;
+  const pizzaCrust = document.getElementById("pizza-crust").value;
+  const pizzaSauce = document.getElementById("pizza-sauce").value;
+  let toppingsArray = [];
+  document.querySelectorAll("input[name=pizza-toppings]:checked").forEach(function(element) {
+    let topping = element['value'];
+    toppingsArray.push(topping);
+  });
+
+}
